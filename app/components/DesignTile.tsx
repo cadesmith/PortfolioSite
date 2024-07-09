e27618c37459ff6bc31a245c4b874d012d767f52
Cade Smith <2cadesmith@gmail.com>
2024-07-09 13:04:50 -0700

    changes to DesignTile

--- app/components/DesignTile.tsx
+++ app/components/DesignTile.tsx
@@ -1,11 +1,19 @@
 import Image from 'next/image'
 import React from 'react'
 
-export default function DesignTile({image, description, title}: any) {
+export default function DesignTile({image, description, title, date}: any) {
   return (
     <div className="grid bg-white text-black w-3/5">
       <div>IMAGE</div>
-      <div>DESCR AND TITLE</div>
+      <div className='grid'>
+        <div className='flex items-center'>
+          <h1>TITLE OF WORK</h1>
+          <h2>DATE</h2>
+        </div>
+        <div>
+          <p>DESCRIPTION</p>
+        </div>
+      </div>
     </div>
   )
 }
