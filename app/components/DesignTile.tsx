07e878d1e7f96ade8601de3441be70fc21af73a4
Cade Smith <2cadesmith@gmail.com>
2024-07-09 12:23:25 -0700

    added bones of graphic design page

--- app/components/DesignTile.tsx
+++ app/components/DesignTile.tsx
@@ -1,7 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
-export default function DesignTile() {
+export default function DesignTile({image, description, title}: any) {
   return (
-    <div>DesignTile</div>
+    <div className="grid bg-white text-black w-3/5">
+      <div>IMAGE</div>
+      <div>DESCR AND TITLE</div>
+    </div>
   )
 }
