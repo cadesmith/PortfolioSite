5310f5efd0d8873b2729bdc2ba7a99a91d99ebd2
Cade Smith <2cadesmith@gmail.com>
2024-09-17 23:06:22 -0700

    added breakpoints on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,8 +7,8 @@ const bebas = Bebas_Neue({ weight: "400", subsets: ["latin"] })
 export default function About() {
     return (
       <main>
-        <div className="flex justify-center">
-          <div className="flex w-1/3"> 
+        <div className="grid justify-items-center xl:flex xl:justify-center">
+          <div className="xl:w-1/3"> 
             <Image 
               src={Portrait}
               alt=''
@@ -18,12 +18,12 @@ export default function About() {
               }}
             />
           </div>
-          <div className="flex flex-col w-1/3 pl-10 mt-auto mb-28"> 
+          <div className="grid mt-10 xl:flex md:flex-col xl:w-1/3 pl-10 xl:mt-auto mb-28"> 
             <div className="">
-              <h1 className={bebas.className}>Hi, I'm Cade.</h1>
+              <h1 className={bebas.className}>ABOUT ME.</h1>
             </div>
             <div className='text-xl'>
-              <p>I'm a developer, photographer, and graphic artist based in the Bay Area. I attended Colgate University in Hamilton, NY
+              <p>I'm a developer, photographer, and graphic artist based in the Bay Area. From 2020-2024 I attended Colgate University in Hamilton, NY
               where I studied computer science and art history. Now I build responsive, effective web applications. When I'm not coding, 
               you can find me hiking, mountain biking, or spending time with my three dogs.
               </p>
