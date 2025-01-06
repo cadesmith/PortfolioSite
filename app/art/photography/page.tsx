f23ddaf4d4ac5883baf0d23e177f8f7bf52a0eea
Cade Smith <2cadesmith@gmail.com>
2025-01-06 13:54:16 -0800

    added text to photography page

--- app/art/photography/page.tsx
+++ app/art/photography/page.tsx
@@ -7,10 +7,10 @@ export default function Photography() {
   return (
     <div className="grid justify-items-center w-screen">
       <div className='pb-6 grid justify-items-center'>
-          <h1 className={`${bebas.className} text-8xl`}>PHOTOGRAPHY</h1>
+        <h1 className={`${bebas.className} text-8xl`}>PHOTOGRAPHY</h1>
       </div>
-      <div> 
-
+      <div>
+        <p>To view all of my photos and purchase prints, visit my photography website:</p>
       </div>
     </div>
   )
