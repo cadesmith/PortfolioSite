4b4961e733d83fda84e1c24503488901aa0d2a26
Cade Smith <2cadesmith@gmail.com>
2025-03-13 21:01:57 -0700

    formatted text box for designtile component

--- app/components/DesignTile.tsx
+++ app/components/DesignTile.tsx
@@ -3,21 +3,21 @@ import React from 'react'
 
 export default function DesignTile({image, description, title, date}: any) {
   return (
-    <div className="grid w-3/5">
+    <div className="grid w-full lg:w-4/5">
       <div className='flex justify-center'>
         IMAGE
       </div>
       <div className='grid bg-white text-black'>
-        <div className='flex justify-between'>
-          <div>
-            <h1>{title}</h1>
+        <div className='flex justify-between mt-4 mx-4'>
+          <div className='w-2/3'>
+            <h1 className='text-5xl'>{title}</h1>
           </div>
-          <div>
-            <h1>{date}</h1>
+          <div className='text-right'>
+            <h1 className='text-5xl'>{date}</h1>
           </div>
         </div>
-        <div>
-          <p>{description}</p>
+        <div className='ml-4 my-4'>
+          <p className='text-xl'>{description}</p>
         </div>
       </div>
     </div>
