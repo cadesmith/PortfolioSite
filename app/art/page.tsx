cfcd63888ab3bae118e70797ddefaf0153da9d66
Cade Smith <2cadesmith@gmail.com>
2025-03-26 11:40:14 -0700

    reformatted mobile margins on art page

--- app/art/page.tsx
+++ app/art/page.tsx
@@ -4,7 +4,7 @@ import PageTitle from '../components/PageTitle';
 
 export default function Art() {
     return (
-      <main className='bg-white 2xl:pb-10'>
+      <main className='bg-white'>
         
         <div className='flex justify-center'> 
           <PageTitle 
@@ -12,7 +12,7 @@ export default function Art() {
           />
         </div>
 
-        <div className='grid 2xl:flex w-full 2xl:space-x-8 2xl:px-16'>
+        <div className='grid space-y-10 pb-10 px-10 w-full 2xl:flex 2xl:space-y-0 2xl:space-x-8 2xl:px-16'>
           
           <ArtTile 
             category={'PHOTOGRAPHY'}
--- app/components/PageTitle.tsx
+++ app/components/PageTitle.tsx
@@ -2,7 +2,7 @@ import { bebas } from "../fonts"
 
 export default function PageTitle({title}: any) {
   return (
-    <div className='w-fit bg-white pt-6 px-6 mt-4 mb-6'>
+    <div className='w-fit pt-6 px-6 mt-4 mb-6'>
         <div className={`${bebas.className} text-8xl block text-black`}>
             {title}
         </div>
