97e0dbc3b486787e6d90bc3092690a3b610c6012
Cade Smith <2cadesmith@gmail.com>
2025-04-02 21:13:01 -0700

    more small changes to designtile

--- app/art/graphic-design/page.tsx
+++ app/art/graphic-design/page.tsx
@@ -5,20 +5,22 @@ import PageTitle from "../../components/PageTitle";
 export default function GraphicDesign() {
     return (
       <main className="bg-white">
-        <div className="grid justify-items-center gap-4 pb-4">
+        <div className="grid justify-items-center pb-8">
           <PageTitle 
-              title={'GRAPHIC DESIGN'}
+                title={'GRAPHIC DESIGN'}
           />
-          <DesignTile
-            title={'Homestead Valley Music Festival Poster'}
-            description={'A Robert Wesley Wilson inspired poster for the annual Homestead Valley Music Festival in Mill Valley, CA. Designed in Adobe Illustrator.'}
-            image={'/Art/Graphic_Design/HVMF_Poster.jpg'}
-          />
-          <DesignTile
-            title={'San Francisco Bridge Poster'}
-            description={'A hypnotic poster of the Golden Gate Bridge in fog. Designed in Adobe Photoshop.'}
-            image={'/Art/fogPoster.jpg'}
-          />
+          <div className="grid justify-items-center space-y-2 lg:space-y-6">
+            <DesignTile
+              title={'Homestead Valley Music Festival Poster'}
+              description={'A Robert Wesley Wilson inspired poster for the annual Homestead Valley Music Festival in Mill Valley, CA. Designed in Adobe Illustrator.'}
+              image={'/Art/Graphic_Design/HVMF_Poster.jpg'}
+            />
+            <DesignTile
+              title={'San Francisco Bridge Poster'}
+              description={'A hypnotic poster of the Golden Gate Bridge in fog. Designed in Adobe Photoshop.'}
+              image={'/Art/fogPoster.jpg'}
+            />
+          </div>
         </div>
       </main>
     );
